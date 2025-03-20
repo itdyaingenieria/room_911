9a1d8cc99a84b77585d77f354824d834130f5b50
DIEGO FERNANDO YAMA ANDRADE <diegoyamaa@gmail.com>
2025-03-20 11:45:58 -0500

    Feat: replace Go Home button with GoHomeDfya component and update registration layout to use AuthenticatedLayout

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,5 +1,5 @@
 <script setup>
-import GuestLayout from '@/Layouts/GuestLayout.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
@@ -21,93 +21,67 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
+    <AuthenticatedLayout>
+
         <Head title="Register" />
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="name" value="Name" />
+                        <form @submit.prevent="submit">
+                            <div>
+                                <InputLabel for="name" value="Name" />
 
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+                                <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required
+                                    autofocus autocomplete="name" />
 
-                <InputError class="mt-2" :message="form.errors.name" />
+                                <InputError class="mt-2" :message="form.errors.name" />
+                            </div>
+
+                            <div class="mt-4">
+                                <InputLabel for="email" value="Email" />
+
+                                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email"
+                                    required autocomplete="username" />
+
+                                <InputError class="mt-2" :message="form.errors.email" />
+                            </div>
+
+                            <div class="mt-4">
+                                <InputLabel for="password" value="Password" />
+
+                                <TextInput id="password" type="password" class="mt-1 block w-full"
+                                    v-model="form.password" required autocomplete="new-password" />
+
+                                <InputError class="mt-2" :message="form.errors.password" />
+                            </div>
+
+                            <div class="mt-4">
+                                <InputLabel for="password_confirmation" value="Confirm Password" />
+
+                                <TextInput id="password_confirmation" type="password" class="mt-1 block w-full"
+                                    v-model="form.password_confirmation" required autocomplete="new-password" />
+
+                                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                            </div>
+
+                            <div class="mt-4 flex items-center justify-end">
+                                <Link :href="route('login')"
+                                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                Already registered?
+                                </Link>
+
+                                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }"
+                                    :disabled="form.processing">
+                                    Register
+                                </PrimaryButton>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
             </div>
-
-            <div class="mt-4">
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel
-                    for="password_confirmation"
-                    value="Confirm Password"
-                />
-
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
-
-                <InputError
-                    class="mt-2"
-                    :message="form.errors.password_confirmation"
-                />
-            </div>
-
-            <div class="mt-4 flex items-center justify-end">
-                <Link
-                    :href="route('login')"
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                    Already registered?
-                </Link>
-
-                <PrimaryButton
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Register
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+        </div>
+    </AuthenticatedLayout>
 </template>
